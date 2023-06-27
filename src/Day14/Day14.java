46d3987fe953979e8af99256aa692035f0e93259
ulukcodewise <uluk0822@gmail.com>
2023-06-27 13:12:57 -0500

    Have done

---
+++ src/Day14/Day14.java
@@ -0,0 +1,24 @@
+package Day14;
+
+import java.util.Scanner;
+
+public class Day14 {
+    public static void main(String[] args) {
+        // break, continue
+        // implement
+        //  1 3 5 7   -> to be skipped
+        // 0 2 4 6 8  -> to include
+        // i p e e t
+        Scanner scanner = new Scanner(System.in);
+
+        // implement
+        // 012345678
+        //  1 3 5 6
+        // iMpLeMeNt
+        // implement
+
+        for(int i = 'a'; i< 'z'; i++){
+            System.out.println((char)i);
+        }
+    }
+}
